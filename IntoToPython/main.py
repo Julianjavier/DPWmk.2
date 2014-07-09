f93bf398d7d83d3e45170d9c4685ba82bb70f37f
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-09 14:37:12 -0400

    Init

---
+++ IntoToPython/main.py
@@ -0,0 +1,11 @@
+# print "HERRO"
+
+
+first_name = "Julian"
+last_name = "Rodriguez"
+
+age = raw_input("AGE?")
+
+print "You are "+ age
+
+
---
+++ Rodriguez_Julian_Madlib/main.py
@@ -0,0 +1,9 @@
+#First Prompt
+print "Well hello hail and harty traveler, welcome to my humble Inn. May I ask your name?"
+
+NAME= raw_input(" Full Name?:")
+print "Ahhh so your name is "+NAME+ " ....I see."
+
+#Second Prompt
+CLASS= raw_input(" What Vocation are you?: _")
+print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
