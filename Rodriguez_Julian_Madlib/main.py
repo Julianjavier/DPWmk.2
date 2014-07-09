753257be1ff94ffc3a7b28755e510b242af62574
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-09 14:39:09 -0400

    Third Prompt

--- Rodriguez_Julian_Madlib/main.py
+++ Rodriguez_Julian_Madlib/main.py
@@ -5,5 +5,9 @@ NAME= raw_input(" Full Name?:")
 print "Ahhh so your name is "+NAME+ " ....I see."
 
 #Second Prompt
-CLASS= raw_input(" What Vocation are you?: _")
+CLASS= raw_input(" What Vocation are you?:_")
 print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
+
+#Third Prompt
+HOME= raw_input("Where do you hail from?:_")
+print "I see, so you come from the land of "+HOME+", How nice."
