aca4cce3203319666034f1ab9cc3d1e66f1213a7
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-09 20:57:25 -0400

    Loop for Beast HP damage.

--- Rodriguez_Julian_Madlib/main.py
+++ Rodriguez_Julian_Madlib/main.py
@@ -1,44 +1,46 @@
 import random
 
-# #First Prompt
-# print "Well hello hail and harty traveler, welcome to my humble Inn. May I ask your name?"
-#
-# NAME= raw_input(" Full Name?:")
-# print "Ahhh so your name is "+NAME+ " ....I see."
-#
-# #Second Prompt
-# CLASS= raw_input(" What Vocation are you?:_")
-# print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
-#
-# #Third Prompt
-# HOME= raw_input("Where do you hail from?:_")
-# print "I see, so you come from the land of "+HOME+", How nice."
-#
-#
-# #Fifth Prompt
-# print "Tell me more about yourself!"
-# STR= raw_input("What is your strength value?")
-# DEX= raw_input("What is your dexterity value?")
-# # LUK= raw_input("How much of a wizard are you?(0-3!)")
-#
-# #stats, class, beasts
-# print "I see, I see... So you have "+STR+" points of STR, "+DEX+" points of DEX and "+LUK+" points LUK."
-#
-# CharacterInfo= {'Name':NAME, 'Class':CLASS, 'Home':HOME}
-#
-# BEASTS= ["Dragon", "Ogre", "Litch", "Hell Hound"]
+#First Prompt
+print "Well hello hail and harty traveler, welcome to my humble Inn. May I ask your name?"
+
+NAME= raw_input(" Full Name?:")
+print "Ahhh so your name is "+NAME+ " ....I see."
+
+#Second Prompt
+CLASS= raw_input(" What Vocation are you?:_")
+print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
+
+#Third Prompt
+HOME= raw_input("Where do you hail from?:_")
+print "I see, so you come from the land of "+HOME+", How nice."
+
+
+#Fifth Prompt
+print "Tell me more about yourself!"
+STR= raw_input("What is your strength value?")
+DEX= raw_input("What is your dexterity value?")
+# LUK= raw_input("How much of a wizard are you?(0-3!)")
+
+#stats, class, beasts
+print "I see, I see... So you have "+STR+" points of STR, "+DEX+" points of DEX and "+LUK+" points LUK."
+
+CharacterInfo= {'Name':NAME, 'Class':CLASS, 'Home':HOME}
+
+BEASTS= ["Dragon", "Ogre", "Litch", "Hell Hound"]
 
 BEAST_HP= random.randint(2000, 9999)
 
-# i = random.randint(1,5)
-#
-#
-# #Print Beast Encounter
-# print "Your adventure begins wile you make your way to an old dungeon were you face a mighty "+BEASTS[i]+"!"
-#
-# def playerDPS(a, b):
-#     DPS = (a * b)
-#     print DPS
-#
-# playerDPS(int(STR), int(DEX))
+i = random.randint(1,5)
+
+
+#Print Beast Encounter
+print "Your adventure begins wile you make your way to an old dungeon were you face a mighty "+BEASTS[i]+"!"
+
+def playerDPS(a, b):
+    DPS = (a * b)
+    while BEAST_HP > 0:
+    BEAST_HP - DPS
+
+
+playerDPS(int(STR), int(DEX))
 
