c9ecdaac4df8d8370a9141617ea84072cab0bce8
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-10 22:32:28 -0400

    loop create

--- Quiz1/main.py
+++ Quiz1/main.py
@@ -2,6 +2,8 @@
 # 7/10/14
 # DPW Quiz1
 
+
+##functions
 def square_area(w,h):
     area= w * h
     return area
@@ -13,3 +15,15 @@ def rectangle_area(w,h):
 print "The area of this square is "+str(square_area(2, 100))
 
 print "The area of this rectangle is "+str(rectangle_area(2, 20))
+
+###loop
+
+bottles = 99
+
+def count_down(a):
+    global bottles
+    bottles = bottles - a
+    return bottles
+
+while bottles > 0:
+    print count_down(1)
