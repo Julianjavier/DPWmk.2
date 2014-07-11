051409d2e705902bb6ef6c0ce1d5821149d47761
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-10 22:08:13 -0400

    Area rectangle function

--- Quiz1/main.py
+++ Quiz1/main.py
@@ -5,3 +5,11 @@
 def square_area(w,h):
     area= w * h
     return area
+
+def rectangle_area(w,h):
+    area= w * h
+    return area
+
+print "The area of a square is "+str(square_area(2, 100))
+
+print "The area of a square is "+str(rectangle_area(2, 20))
