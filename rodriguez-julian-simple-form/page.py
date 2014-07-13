dc9da1edb0b0bba33b2ca12bcb80ee482668d980
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-12 23:00:43 -0400

    select list test

--- rodriguez-julian-simple-form/page.py
+++ rodriguez-julian-simple-form/page.py
@@ -23,5 +23,11 @@ class main_page(object):
             <input type="radio" name="rating" value="4">
         </div>
 
+        <select name="select">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+
         </form>
         '''
