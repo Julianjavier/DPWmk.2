40377abb2f09729327d1212976b05e131ae3fc44
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-07-12 21:02:57 -0400

    Main HTML page class

---
+++ rodriguez-julian-simple-form/page.py
@@ -0,0 +1,13 @@
+
+class main_page(object):
+    def __init__(self):
+        self.page_open='''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="wrap">
+        '''
